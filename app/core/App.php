57747233e62205e965783b9a21a94876562da869
supardi9512 <supardi9512@gmail.com>
2020-08-08 22:57:40 +0700

    membuat routing

--- app/core/App.php
+++ app/core/App.php
@@ -3,6 +3,20 @@
 class App {
     public function __construct()
     {
-        echo 'OK!';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+    
+    public function parseURL()
+    {
+        if(isset($_GET['url'])) {
+            // menghapus tanda '/' paling akhir dari url
+            $url = rtrim($_GET['url'], '/');
+            // membersihkan url dari karakter aneh
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // memecahkan url berdasarkan tanda slash '/'
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
